b10030ef38e7ab0b065b3d5ba334bfecb2792b0e
tschilpi <marios.vettas@bluewin.ch>
2020-04-30 09:34:13 -0400

    Auth contract, oz cli % ganache-cli

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -5,7 +5,7 @@ contract Box {
 	uint256 private value;
 
 	// Emitted when the stored value changes
-	event ValueChanged(uint 256 newValue);
+	event ValueChanged(uint256 newValue);
 
 	// Stores a new value in the contract
 	function store(uint256 newValue) public {
---
+++ contracts/access-control/Auth.sol
@@ -0,0 +1,15 @@
+// contracts/access-control/Auth.sol
+pragma solidity ^0.5.0;
+
+contract Auth {
+	address private administrator;
+
+	constructor () public {
+		// Make the deployer of the contract the administrator
+		administrator = msg.sender;
+	}
+
+	function isAdministrator(address user) public view returns (bool) {
+		return user == administrator;
+	}
+}
